9ad2d414d1aa4ec41b0b89382a89ca939422389e
Artemivashenko <artemivashenko333@gmail.com>
2022-08-12 16:03:10 +0300

    calendar

--- script.js
+++ script.js
@@ -1,31 +1,70 @@
  const date = new Date();
 
+
 function calendar(){
+
+
 //const month = date.getMonth(+1);
 const month =["Январь","Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь" , "Октябрь", "Ноябрь", "Декабрь"];
 
 const lastDay = new Date(date.getFullYear(), date.getMonth()+1, 0).getDate();
+
+const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
 console.log(lastDay);
 let monthDays = document.querySelector('.days');
 document.querySelector(".date h1").innerHTML = month[date.getMonth()];
 document.querySelector(".date p").innerHTML = date.toLocaleDateString();
 
 let days =  "";
+const day = date.getDay()+1;
+
+const next = new Date(date.getFullYear(), date.getMonth()+1, 0).getDay();
+
+
+
+const firstDayMonth = day;
+console.log(firstDayMonth);
+console.log(date.getDay().lastDay);
+
+let lastDayMonth = 7-next;
+
+console.log(day);
+console.log(date.getDate());
+
+for (let i=day; i>=0; i--){
+	days+=`<div> ${prevLastDay-i} </div>`;
+	monthDays.innerHTML = days;
+}
 
 
 for (let i = 1; i<=lastDay; i++){
+	if(i===date.getDate() && i===date.getMonth()){
+		days+=`<div class = "today"> ${i} </div>`;
+monthDays.innerHTML = days;
+}else{
 	days+= ` <div >${i} </div>`;
 	monthDays.innerHTML = days;
 	
-
 }
+}
+
  
-let firstDayMonth = date.getDay();
-console.log(firstDayMonth);
+	for (let i =1;  i<=lastDayMonth; i++) {
+		
+
+	 
+	days+= `<div> ${i} </div>`;
+	monthDays.innerHTML = days;
+	 
+}
 
 
 
 }
+
+
+
+
 const  nextmonth = document.querySelector('[date-next]');
  nextmonth.addEventListener('click' , button =>{
 date.setMonth(date.getMonth()+1);
@@ -40,3 +79,4 @@ const  prevmonth = document.querySelector('[date-prev]');
 
 
 calendar();
+
