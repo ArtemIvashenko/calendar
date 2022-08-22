84345c0a41586b580248648b514cfc5a904225d3
Artemivashenko <artemivashenko333@gmail.com>
2022-08-22 16:39:52 +0300

    22.08 clock

--- script.js
+++ script.js
@@ -1,109 +1,104 @@
 "use strict";
-let date = new Date();
+
 class Calendar {
-    constructor() {
-       
+
+    constructor(dateObject, month) {
+        this.dateObject = dateObject;
+        this.month = month;
     }
 
-    
+    process() {
+        let lastDay = new Date(this.dateObject.getFullYear(), this.dateObject.getMonth() + 1, 0).getDate();
+        let prevLastDay = new Date(this.dateObject.getFullYear(), this.dateObject.getMonth(), 0).getDate();
+        let monthDays = document.querySelector('.days');
+        document.querySelector('.date h1').innerHTML = this.month[this.dateObject.getMonth()];
+        document.querySelector('.date p').innerHTML = this.dateObject.toLocaleDateString();
 
-    calendarRun(){
- let month =[
-        "Январь",
-        "Февраль",
-        "Март",
-        "Апрель",
-        "Май",
-        "Июнь",
-        "Июль",
-        "Август",
-        "Сентябрь",
-        "Октябрь",
-        "Ноябрь",
-        "Декабрь"
-        ];
-
-let lastDay = new Date(date.getFullYear(), date.getMonth()+1, 0).getDate();
-let prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
-let monthDays = document.querySelector('.days');
-document.querySelector('.date h1').innerHTML = month[date.getMonth()];
-document.querySelector('.date p').innerHTML = date.toLocaleDateString();
-
-let days = '';
-let day = date.getDay()+1;
-let next = new Date(date.getFullYear(), date.getMonth()+1, 0).getDay();
-let lastDayMonth = 7-next;
-   
-    for (let i = day+1; i>0; i--){
-        days+=`<div> ${prevLastDay-i+1} </div>`;
-        monthDays.innerHTML = days;
-    }
+        let days = '';
+        let day = this.dateObject.getDay() ;
+        day = day - 1;
+        let next = new Date(this.dateObject.getFullYear(), this.dateObject.getMonth() + 1, 0).getDay();
+        let lastDayMonth = 7 - next;
+            if (day <0) {
+                day= 6;
+            } 
+                for (let i = day ; i > 0; i--) {
+                days +=` <div> ${prevLastDay - i + 1 } </div>`;
+                monthDays.innerHTML = days;
+                }
 
 
-    for (let i = 1; i<=lastDay; i++){
-        if(i===date.getDate() && 
-            date.getMonth() === new Date().getMonth() &&
-            date.getFullYear() === new Date().getFullYear()){
-            days+=`<div class = 'today'> ${i} </div>`;
+        for (let i = 1; i <= lastDay; i++) {
+            if (i === this.dateObject.getDate() &&
+                this.dateObject.getMonth() === new Date().getMonth() &&
+                this.dateObject.getFullYear() === new Date().getFullYear()) {
+                days += `<div class = 'today'> ${i} </div>`;
+                monthDays.innerHTML = days;
+            } else {
+                days += ` <div class = 'thismonth' > ${i} </div>`;
+                monthDays.innerHTML = days;
+
+            }
+        }
+
+
+        for (let i = 1; i <= lastDayMonth; i++) {
+            days += `<div> ${i} </div>`;
             monthDays.innerHTML = days;
-        }else{
-            days+= ` <div class = 'thismonth' > ${i} </div>`;
-	    monthDays.innerHTML = days;
-	
-        }  
-    } 
 
- 
-    for (let i =1;  i<=lastDayMonth; i++) {	 
-        days+= `<div> ${i} </div>`;
-        monthDays.innerHTML = days;
-	 
+        }
     }
 
-    }    
-}
-
-
-let calendar = new Calendar();
-
-calendar.calendarRun();
-let  nextmonth = document.querySelector('[date-next]');
-    nextmonth.addEventListener('click' , button =>{
-    date.setMonth(date.getMonth()+1);
-    calendar.calendarRun();
-    });
-
-let  prevmonth = document.querySelector('[date-prev]');
-    prevmonth.addEventListener('click' , button =>{
-    date.setMonth(date.getMonth()-1);
-    calendar.calendarRun();
-    });
-
-    function  clockcalendar() {
-	let date = new Date();
+    clock(date) {
         let h = date.getHours().toString();
         let m = date.getMinutes().toString();
         let s = date.getSeconds().toString();
-            if (h.length<2) {
-	        h = '0' + h;
-            }
-            if (m.length<2) {
-	        m = '0' + m;
-            }
-            if (s.length<2) {
-                s = '0' + s;
+
+        if (h.length < 2) {
+            h = '0' + h;
         }
-        let clockt = h + ':' + m + ':' + s;
+        if (m.length < 2) {
+            m = '0' + m;
+        }
+        if (s.length < 2) {
+            s = '0' + s;
+        }
 
-	document.querySelector('[ time]').innerHTML = clockt;
-
-      
+        return h + ':' + m + ':' + s;
     }
+}
 
-clockcalendar();
-setInterval(clockcalendar, 1000);
+const month = [
+    "Январь",
+    "Февраль",
+    "Март",
+    "Апрель",
+    "Май",
+    "Июнь",
+    "Июль",
+    "Август",
+    "Сентябрь",
+    "Октябрь",
+    "Ноябрь",
+    "Декабрь"
+];
+let date = new Date();
+let calendar = new Calendar(date, month);
 
+calendar.process();
 
+let nextmonth = document.querySelector('[date-next]');
+nextmonth.addEventListener('click', button => {
+    date.setMonth(date.getMonth() + 1);
+    calendar.process();
+});
 
+let prevmonth = document.querySelector('[date-prev]');
+prevmonth.addEventListener('click', button => {
+    date.setMonth(date.getMonth() - 1);
+    calendar.process();
+});
 
-
+setInterval(function () {
+    document.querySelector('[ time]').innerHTML = calendar.clock(new Date())
+}, 1000);
